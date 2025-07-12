8cbfd3080d8fc10ca929b6d6fe678f89442bff55
Michael Reilly <OmnipotentEntity@gmail.com>
2025-07-12 01:05:33 -0500

    Removed cluttered messages

--- Jokers/hivemind.lua
+++ Jokers/hivemind.lua
@@ -28,44 +28,11 @@ SMODS.Joker {
           end
         end
 
-        -- If we saw ourselves and at least one other friend
         if #_cards > 1 then
           return {
-            -- Joker says As One!
-            message = "As One!",
+            message = localize('k_again_ex'),
             repetitions = self.config.extra.repetitions,
-            card = context.self,
-            func = function()
-              -- A pared down version of card_eval_status_text designed to 
-              -- juice up multiple cards at once
-              -- This makes all of the cards that match say "Again!" at the same
-              -- time
-              percent = 0.9 + 0.2*math.random()
-              y_off = -0.05*G.CARD_H
-              G.E_MANAGER:add_event(Event({ --Add bonus chips from this card
-                trigger = 'before',
-                delay = 0.75,
-                blocking = nil,
-                blockable = nil,
-                func = function()
-                  for i=1, #_cards do
-                    attention_text({
-                      text = localize('k_again_ex'),
-                      scale = 1, 
-                      hold = 0.7375,
-                      backdrop_colour = G.C.FILTER,
-                      align = 'tm',
-                      major = _cards[i],
-                      offset = {x = 0, y = y_off}
-                    })
-                    _cards[i]:juice_up(0.6, 0.1)
-                  end
-                  G.ROOM.jiggle = G.ROOM.jiggle + 0.7
-                  play_sound(sound, 0.8+percent*0.2, 1)
-                  return true
-                end
-              }))
-            end
+            card = context.self
           }
         end
       end
