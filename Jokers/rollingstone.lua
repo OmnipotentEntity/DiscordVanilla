9f5231906448e952cfcc83756595c3c4619df59a
Michael Reilly <OmnipotentEntity@gmail.com>
2025-08-21 23:58:33 -0500

    Reimpementation of Rolling Stone, should avoid most of the bugs

--- Jokers/rollingstone.lua
+++ Jokers/rollingstone.lua
@@ -1,3 +1,26 @@
+local old_calc_reroll = calculate_reroll_cost
+
+calculate_reroll_cost = function(skip_increment)
+  if next(SMODS.find_card('j_uvdm_rollingstone')) then
+    if G.shop_jokers and G.shop_jokers.cards then
+      local free = true
+      for i=1,#G.shop_jokers.cards do
+        if G.shop_jokers.cards[i].ability.set == "Joker" then
+          free = false
+          break
+        end
+      end
+
+      if free then
+        G.GAME.current_round.reroll_cost = 0
+        return
+      end
+    end
+  end
+
+  old_calc_reroll(skip_increment)
+end
+
 SMODS.Joker {
   key = "rollingstone",
   loc_txt = {
@@ -16,37 +39,8 @@ SMODS.Joker {
   pos = { x = 5, y = 0 },
   cost = 6,
   calculate = function(self, card, context)
-    if context.reroll_shop or context.starting_shop then
-      G.rolling_stone_activated_this_reroll = false
-    end
-
-    if context.shop_update or context.reroll_shop or context.starting_shop then
-      local free = true
-      for i=1,#G.shop_jokers.cards do
-        if G.shop_jokers.cards[i].ability.set == "Joker" then
-          free = false
-          break
-        end
-      end
-
-      if free and not G.rolling_stone_activated_this_reroll then
-        G.GAME.current_round.free_rerolls = math.max(G.GAME.current_round.free_rerolls + 1, 0)
-        calculate_reroll_cost(true)
-        G.rolling_stone_activated_this_reroll = true
-      end
-
-      -- This can occur when purchasing the overstock voucher, a Joker appears
-      -- in a vacant slot, deactivating the free reroll we added
-      if not free and G.rolling_stone_activated_this_reroll then
-        G.GAME.current_round.free_rerolls = math.max(G.GAME.current_round.free_rerolls - 1, 0)
-        calculate_reroll_cost(true)
-        G.rolling_stone_activated_this_reroll = false
-      end
-    end
-
-    if context.ending_shop and G.rolling_stone_activated_this_reroll then
-      G.GAME.current_round.free_rerolls = math.max(G.GAME.current_round.free_rerolls - 1, 0)
-      G.rolling_stone_activated_this_reroll = false
+    if context.shop_update then
+      calculate_reroll_cost(true)
     end
   end
 }
